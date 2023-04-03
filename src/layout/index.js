9c237aaa3a5e26cf7a24592eab644294c1fb34d1
VocHub <payomide404@gmail.com>
2023-04-03 23:59:15 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/layout/index.js
@@ -0,0 +1,28 @@
+import { Footer, Navbar } from "@/components";
+import Head from "next/head";
+import PropTypes from "prop-types";
+
+const Layout = ({ children }) => (
+  <>
+    <Head>
+      <title>Kokunime</title>
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
+    </Head>
+    <div className="min-h-screen flex flex-col justify-between">
+      <Navbar />
+      <main className="min-h-full" id="particle-js">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  </>
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
